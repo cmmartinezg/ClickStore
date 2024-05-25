dc947da078cb229cf2ae375dab409535603477e2
cmmartinezg <160249987+cmmartinezg@users.noreply.github.com>
2024-05-25 03:11:57 -0400

    api

---
+++ ClickStore/js/api.js
@@ -0,0 +1,22 @@
+// js/api.js
+
+async function obtenerProductos() {
+    try {
+      const response = await fetch('http://localhost:3000/api/productos');
+      const productos = await response.json();
+      const lista = document.getElementById('productos-lista');
+      lista.innerHTML = ''; // Limpiar la lista antes de agregar nuevos elementos
+      productos.forEach(producto => {
+        const li = document.createElement('li');
+        li.textContent = `${producto.nombre}: ${producto.descripcion} - ${producto.precio} Gs`;
+        lista.appendChild(li);
+      });
+    } catch (error) {
+      console.error('Error al obtener productos:', error);
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    obtenerProductos();
+  });
+  
